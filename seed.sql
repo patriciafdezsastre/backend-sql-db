28307b3671ec53878bac35851e3c67e3f577930e
aserrano3198 <56276822+aserrano3198@users.noreply.github.com>
2022-05-06 14:19:44 +0200

    add tarifas db

--- seed.sql
+++ seed.sql
@@ -1,3 +1,5 @@
 INSERT INTO `users_database`.`vehiculos` (`id`, `aparcadook`, `latitud`, `libre`, `longitud`, `tipo`) VALUES ('1', 1, '40.45315837994751', 1, '-3.7266484767199968', 'patinete');
 INSERT INTO `users_database`.`vehiculos` (`id`, `aparcadook`, `latitud`, `libre`, `longitud`, `tipo`) VALUES ('2', 1, '40.44825417535705', 1, '-3.727239427533021', 'bike');
 INSERT INTO `users_database`.`vehiculos` (`id`, `aparcadook`, `latitud`, `libre`, `longitud`, `tipo`) VALUES ('3', 1, '40.45103467433095', 0, '-3.7286027050636923', 'bike');
+INSERT INTO `users_database`.`tarifas` (`nombre_tarifa`, `tarifa`) VALUES ('bike', '0.2');
+INSERT INTO `users_database`.`tarifas` (`nombre_tarifa`, `tarifa`) VALUES ('patinete', '0.3');
